69ecad5f427819d64f593e811ab5d194eb01bb98
PhotonFlow <zceexpe@ucl.ac.uk>
2026-01-18 23:11:22 +0800

    update 20260118-2

--- models/sam_mask_generator.py
+++ models/sam_mask_generator.py
@@ -1,5 +1,40 @@
 import torch 
 import cv2
 import numpy as np
+from segment_anything import sam_model_registry, SamPredictor
+
+
+
+class SAMMaskGenerator:
+    def __init__(self, checkpoint_path="sam_vit_h_4b8939.pth",model_type="vit_h", device="cuda"):
+        self.device = device
+        sam=sam_model_registry[model_type](checkpoint=checkpoint_path)
+        sam.to(device=device)
+        self.predictor = SamPredictor(sam)
+
+
+
+    def generate_mask(self,image, boxes):
+        self.predictor.set_image(image) # [H,W,3]
+        if isinstance(boxes, torch.Tensor):  # [N, 4]
+            boxes=boxes.cpu().numpy()
+        transformed_boxes=self.predictor.transform.apply_boxes_torch(
+            torch.tensor(boxes, device=self.device), image.shape[:2]
+            )
+        masks,_,_=self.predictor.predict_torch(
+            point_coords=None,
+            point_labels=None,
+            boxes=transformed_boxes,
+            multimask_output=False,
+        )
+        return masks.squeeze(1)  # [N, H, W]
+
+
+
+
+
+
+
+
 
 
